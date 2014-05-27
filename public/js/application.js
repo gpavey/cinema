fdc829b28be99db7b6904bf40f28bca68a3aac81
Geoff Pavey <geoff.pavey@gmail.com>
2014-05-26 22:50:53 -0700

    got the data to be returned as a hash and was able to plot on the map

--- public/js/application.js
+++ public/js/application.js
@@ -1,34 +1,34 @@
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-var locations = [
-  ['Dev Boot Camp', 37.784633, -122.397414],
-  ['China Town', 37.794138, -122.407791],
-  ['Ferry Building', 37.795923, -122.392052],
-  ['Golden Gate Bridge', 37.819877, -122.478939],
-  ['Presidio', 37.798874, -122.466194]
-];
+// var locations = [
+//   ['Dev Boot Camp', 37.784633, -122.397414],
+//   ['China Town', 37.794138, -122.407791],
+//   ['Ferry Building', 37.795923, -122.392052],
+//   ['Golden Gate Bridge', 37.819877, -122.478939],
+//   ['Presidio', 37.798874, -122.466194]
+// ];
 
-var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-    '<div id="bodyContent">'+
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the '+
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    'Aboriginal people of the area. It has many springs, waterholes, '+
-    'rock caves and ancient paintings. Uluru is listed as a World '+
-    'Heritage Site.</p>'+
-    '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-    '(last visited June 22, 2009).</p>'+
-    '</div>'+
-    '</div>';
+// var contentString = '<div id="content">'+
+//     '<div id="siteNotice">'+
+//     '</div>'+
+//     '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+//     '<div id="bodyContent">'+
+//     '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+//     'sandstone rock formation in the southern part of the '+
+//     'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+//     'south west of the nearest large town, Alice Springs; 450&#160;km '+
+//     '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+//     'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+//     'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+//     'Aboriginal people of the area. It has many springs, waterholes, '+
+//     'rock caves and ancient paintings. Uluru is listed as a World '+
+//     'Heritage Site.</p>'+
+//     '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+//     'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+//     '(last visited June 22, 2009).</p>'+
+//     '</div>'+
+//     '</div>';
 
 function initialize() {
   // map options
@@ -41,21 +41,46 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
   // set markers
-  setMarkers(map,locations);
+  // setMarkers(map,locations);
+  $('.add_movies').on('click', addMarkers)
+
+  function addMarkers(e){
+    var result
+    e.preventDefault();
+
+    var requestAjax = $.ajax({
+      url: '/locations',
+      type: 'GET'
+      // success: function(data){
+      //  //console.log(data);
+      //  this.result = data;
+      // }
+      })
+      requestAjax.done(test);
+      requestAjax.fail(showErrors);
+  }
+function test(x){
+  result = x
+  setMarkers(map,result)
+}
+  function showErrors(jqXHR, textStatus, errorThrown){
+  alert('what the fuck')
+}
 }
 
 function setMarkers(map,local){
   // set multiple marker
+  debugger;
   for (var i = 0; i < local.length; i++){
     // initialize markers
     var site = local[i];
-    var myLatLng = new google.maps.LatLng(site[1],site[2]);
+    var myLatLng = new google.maps.LatLng(site.lat,site.lng);
     var marker = new google.maps.Marker({
       map: map,
       draggable: false,
       animation: google.maps.Animation.DROP,
       position: myLatLng,
-      title: site[0],
+      title: site.title,
     });
 
     (function(marker,i){
